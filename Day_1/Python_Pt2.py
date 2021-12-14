f611b2cc06504de251dbc17d6ddd4843b53eeb33
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-14 11:00:19 +0000

    Added i -> i + 3 and printed out on testInput

--- Day_1/Python_Pt2.py
+++ Day_1/Python_Pt2.py
@@ -5,3 +5,23 @@ f = open(file_location)
 input = f.read()
 inputSplit = input.splitlines()
 
+testInput = ['100', '101', '105', '106', '103', '104', '106', '108', '112', '123', '125', '149']
+
+
+windowA = 0
+windowB = 0
+# While i < length - 3
+for i in range (len(testInput) - 2):
+    num1 = int(testInput[i])
+    num2 = int(testInput[i + 1])
+    num3 = int(testInput[i + 2])
+    print(num1 + num2 + num3)
+    
+# Add i, i + 1 and i + 2
+# Put into window 1
+
+# Increase i by 3
+# Add i, i + 1 and i + 2
+# Put into window 2
+
+# Compare if A is less than B
