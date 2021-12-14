20b55e1c2882425b541cc409635fe7c1224e3d56
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-14 13:17:37 +0000

    Setting Configs

--- Day_2/Main.java
+++ Day_2/Main.java
@@ -1,7 +1,9 @@
 package Day_2;
+import java.io.*;
 
 class Main {
     public static void main(String[] args) {
-      System.out.println("Hello world!");
+      File fileName = new File("input.txt");
+      System.out.println(fileName);
     }
   }
