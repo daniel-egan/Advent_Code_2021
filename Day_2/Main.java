56bb7cb91b6cf90cdb9629d77ff9d11cce228599
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-14 16:48:26 +0000

    Input and Splitting Working

--- Day_2/Main.java
+++ Day_2/Main.java
@@ -2,8 +2,26 @@ package Day_2;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) {
-      File fileName = new File("input.txt");
-      System.out.println(fileName);
+    public static void main(String[] args) throws IOException{
+
+      File f = new File("input.txt");
+      String filePath = f.getCanonicalPath();
+
+      filePath = filePath.substring(0, filePath.length() - 9);
+      filePath = filePath + "Day_2/input.txt";
+
+      BufferedReader fileInput = new BufferedReader(new FileReader(filePath));
+        for (String line; (line = fileInput.readLine()) != null;) {
+            String split[] = line.split(" ", 0);
+            System.out.println("Direction :");
+            System.out.println(split[0]);
+            System.out.println("Distance :");
+            System.out.println(split[1]);
+
+        }
+      
+
+      
+
     }
   }
