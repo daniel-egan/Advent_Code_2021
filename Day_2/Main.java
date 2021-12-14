6879216067b08fa69ff3be29adca82e4954ad90b
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-14 17:01:11 +0000

    Part 1 Completed

--- Day_2/Main.java
+++ Day_2/Main.java
@@ -10,17 +10,37 @@ class Main {
       filePath = filePath.substring(0, filePath.length() - 9);
       filePath = filePath + "Day_2/input.txt";
 
+      String split[];
+      int vertical = 0;
+      int horizontal = 0;
+
       BufferedReader fileInput = new BufferedReader(new FileReader(filePath));
         for (String line; (line = fileInput.readLine()) != null;) {
-            String split[] = line.split(" ", 0);
-            System.out.println("Direction :");
-            System.out.println(split[0]);
-            System.out.println("Distance :");
-            System.out.println(split[1]);
-
+            split = line.split(" ", 0);
+            if (split[0].equals("up")){
+              int subtraction = Integer.parseInt(split[1]);
+              vertical = vertical - subtraction;
+            }
+            else if (split[0].equals("down")){
+              int addition = Integer.parseInt(split[1]);
+              vertical = vertical + addition;
+            }
+            else if (split[0].equals("forward")){
+              int addition = Integer.parseInt(split[1]);
+              horizontal = horizontal + addition;
+            }
+            else{
+              System.out.println(split[0]);
+              break;
+            }
         }
       
+        System.out.println("Vertical = " + vertical);
+        System.out.println("Horizontal = " + horizontal);
 
+        int multiplication = vertical * horizontal;
+
+        System.out.println("Multiplication = " + multiplication);
       
 
     }
