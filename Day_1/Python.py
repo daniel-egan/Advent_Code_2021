0c7eae142e711eac857351ae14aadb8f346878ab
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-14 10:35:24 +0000

    Completed

--- Day_1/Python.py
+++ Day_1/Python.py
@@ -3,8 +3,16 @@ THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 file_location = os.path.join(THIS_FOLDER, 'input.txt')
 f = open(file_location)
 input = f.read()
-
-
-
 inputSplit = input.splitlines()
+
 print(inputSplit)
+
+increased = 0
+previous = 0
+
+for i in inputSplit:
+    current = int(i)
+    increased = increased + (current > previous)
+    previous = current
+    
+print(increased - 1)
