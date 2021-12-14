4cec65e564f5b544752cd091eab4db0aaf743823
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-14 10:39:35 +0000

    Finished Pt1. Starting Pt2

---
+++ Python_Pt2.py
@@ -0,0 +1,7 @@
+import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+file_location = os.path.join(THIS_FOLDER, 'input.txt')
+f = open(file_location)
+input = f.read()
+inputSplit = input.splitlines()
+
