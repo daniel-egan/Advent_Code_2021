597018a22ba6fd8961becff971780d228e84882b
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-15 20:11:00 +0000

    Runs through input.txt. Prints out the number of lines

--- Day_3/Program.cs
+++ Day_3/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace HelloWorld
+﻿using System.IO;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           string line;
+           int numOfLines = 0;
+           //Pass the file path and file name to the StreamReader constructor
+           StreamReader fileText = new StreamReader("input.txt");
+            //Read the first line of text
+            line = fileText.ReadLine();
+
+            while (line != null)
+            {
+                //Read the next line
+                line = fileText.ReadLine();
+                //Add one to the numOfLines
+                numOfLines += 1;
+            }
+
+            Console.WriteLine(numOfLines);
+
         }
     }
 }
