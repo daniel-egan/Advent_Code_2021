e3bb62565732476c3e583e9f27b180211e3a3c93
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-15 20:52:16 +0000

    Printing out gammaRate and epsilonRate

--- Day_3/Program.cs
+++ Day_3/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System.IO;
 
-namespace HelloWorld
+namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-           string line;
-           int numOfLines = 0;
-           //Pass the file path and file name to the StreamReader constructor
-           StreamReader fileText = new StreamReader("input.txt");
+            string line;
+            int numOfLines = 0;
+            //Pass the file path and file name to the StreamReader constructor
+            StreamReader fileText = new StreamReader("input.txt");
             //Read the first line of text
             line = fileText.ReadLine();
 
@@ -20,9 +20,79 @@ namespace HelloWorld
                 //Add one to the numOfLines
                 numOfLines += 1;
             }
+            //Close file and reopen to restart at first line
+            fileText.Close();
+            fileText = new StreamReader("input.txt");
+            // Create Array with size numOfLines
+            int[] fileInput = new int[numOfLines];
+            // Create string for the final value. This will be appended to before the for loop restarts to get the most common bit of each line
+            string gammaRate = "";
 
-            Console.WriteLine(numOfLines);
+            //Check the first character add one to the corresponding int for 1 or 0
+            //Go to next line
+            //Do for all 12 characters
 
+            for (int i = 0; i < 12; i++)
+            {
+                int NumOf0 = 0;
+                int NumOf1 = 0;
+                line = fileText.ReadLine();
+
+                while (line != null)
+                {
+                    //Split the line into characters 
+                    char[] characters = line.ToCharArray();
+                    //Add one to the int for 1(49) or 0(48)
+                    if (characters[i] == 48)
+                    {
+                        NumOf0 += 1;
+                    }
+
+                    else if (characters[i] == 49)
+                    {
+                        NumOf1 += 1;
+                    }
+
+                    //Read the next line
+                    line = fileText.ReadLine();
+                }
+
+                //Check which is more value is more common
+
+                if (NumOf0 < NumOf1)
+                {
+                    gammaRate += "1";
+                }
+                else
+                {
+                    gammaRate += "0";
+                }
+
+
+                //Close file and reopen to restart at first line
+                fileText.Close();
+                fileText = new StreamReader("input.txt");
+            }
+
+            //Calculate epsilon rate by flipping 1 to 0 and 0 to 1
+            string epsilonRate = "";
+
+            char[] gammaRateArray = gammaRate.ToCharArray();
+
+            for (int i = 0; i < 12; i++)
+            {
+                int flipValue = gammaRateArray[i];
+                if (flipValue == 49)
+                {
+                    epsilonRate += "0";
+                }
+                else
+                {
+                    epsilonRate += "1";
+                }
+            }
+            Console.WriteLine(gammaRate);
+            Console.WriteLine(epsilonRate);
         }
     }
 }
