3b5abb4a69034240ee8a89ce58650be1b7f1308b
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-19 20:35:37 +0000

    Started Rust "Hello, world!"

---
+++ Day_4/target/debug/Day_4.d
@@ -0,0 +1,1 @@
+c:\Users\danie\Documents\Advent_Code_2021\Day_4\target\debug\Day_4.exe: c:\Users\danie\Documents\Advent_Code_2021\Day_4\src\main.rs
---
+++ Day_4/target/debug/deps/Day_4.d
@@ -0,0 +1,5 @@
+c:\Users\danie\Documents\Advent_Code_2021\Day_4\target\debug\deps\Day_4.exe: src\main.rs
+
+c:\Users\danie\Documents\Advent_Code_2021\Day_4\target\debug\deps\Day_4.d: src\main.rs
+
+src\main.rs:
