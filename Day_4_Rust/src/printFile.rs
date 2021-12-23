5870f7ab5de2bda273923e0b033fc8e63399ba99
Daniel <daniel.egan@fairgroundtown.co.uk>
2021-12-23 13:00:16 +0000

    Currently gets all the values from boardsVec and splits them into seperate values (value1, value2). These can then be converted into int. This is done for the entire board.

--- Day_4_Rust/src/printFile.rs
+++
@@ -1,7 +0,0 @@
-pub fn run() {
-    use std::io::Read;
-    let mut file = std::fs::File::open("input.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    print!("{}", contents);
-}
